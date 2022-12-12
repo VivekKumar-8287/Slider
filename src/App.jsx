97ac408de48f575c03bef6b8be6ed61d355c9063
Vivek Kumar <104918205+VivekKumar-8287@users.noreply.github.com>
2022-12-12 15:12:26 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h2>slider project setup</h2>
+    </main>
+  )
+}
