64dc4115179eeaa0f69d8dda2a73d53311ba2337
Vivek Kumar <104918205+VivekKumar-8287@users.noreply.github.com>
2022-12-13 14:56:48 +0000

    Added the usestate and call data from data.js, using map function to use the data.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,43 @@ import data from './data';
 import './App.css'
 
 export default function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
   return (
-    <main>
-      <h2>slider project setup</h2>
-    </main>
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          // more stuff coming up
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (personIndex === index - 1) {
+            position = 'lastSlide'      //2:38:32
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   )
 }
